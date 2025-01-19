d5e615aee437067a031b2f0753b507df577ba80d
LilleAila <olai.solsvik@gmail.com>
2025-01-19 18:31:18 +0100

    snippets: better delimiters, close #13

--- config/snippets/lua/lilleaila-snippets/snippets/tex_math/delimiters.lua
+++ config/snippets/lua/lilleaila-snippets/snippets/tex_math/delimiters.lua
@@ -10,56 +10,57 @@ local _masnip = tex._masnip
 -- brackets
 -- maybe rename to use characters instead of symbols?
 local brackets = {
-  ["("] = { "(", ")" },
-  ["["] = { "[", "]" },
-  ["{"] = { "\\{", "\\}" },
+  ["p"] = { "(", ")" },
+  ["b"] = { "[", "]" },
+  ["B"] = { "\\{", "\\}" },
   ["<"] = { "\\langle", "\\rangle" },
   ["|"] = { "|", "|" },
   ["d"] = { "\\|", "\\|" },
   ["c"] = { "\\lceil", "\\rceil" },
   ["f"] = { "\\lceil", "\\rceil" },
+  ["."] = { ".", "." },
 }
 
+local fallback_bracket = "p"
+
+local function escapePattern(s)
+  return s:gsub("([%.%%%+%-%*%?%[%]%^%$%(%)])", "%%%1")
+end
+
+local bracketsMatch = "([" .. table.concat(vim.tbl_map(escapePattern, vim.tbl_keys(brackets))) .. "])"
+
 M = {
-  _masnip({ trig = "lr([%(%[{<|dcf])", name = "left right", dscr = "left right delimiters", regTrig = true },
+  _masnip({ trig = "lr" .. bracketsMatch, name = "Left and right equal delimiters", regTrig = true },
     fmta(
       [[
       \left<> <> \right<><>
     ]],
       {
         f(function(_, snip)
-          local cap = snip.captures[1] or '('
+          local cap = snip.captures[1] or fallback_bracket
           return brackets[cap][1]
         end),
         d(1, utils.get_visual),
         f(function(_, snip)
-          local cap = snip.captures[1] or '('
+          local cap = snip.captures[1] or fallback_bracket
           return brackets[cap][2]
         end),
         i(0)
       })),
-  _masnip({ trig = "l([%(%[{<|dcf])", name = "left", dscr = "left delimiters", regTrig = true },
+  _masnip(
+    { trig = "l" .. bracketsMatch .. "r" .. bracketsMatch, name = "Left and right non-equal delimiters", regTrig = true },
     fmta(
       [[
-      \left<> <> \right.<>
+      \left<> <> \right<><>
     ]],
       {
         f(function(_, snip)
-          local cap = snip.captures[1] or '('
+          local cap = snip.captures[1] or fallback_bracket
           return brackets[cap][1]
         end),
         d(1, utils.get_visual),
-        i(0)
-      })),
-  _masnip({ trig = "r([%(%[{<|dcf])", name = "right", dscr = "right delimiters", regTrig = true },
-    fmta(
-      [[
-      \left. <> \right<><>
-    ]],
-      {
-        d(1, utils.get_visual),
         f(function(_, snip)
-          local cap = snip.captures[1] or '('
+          local cap = snip.captures[2] or fallback_bracket
           return brackets[cap][2]
         end),
         i(0)
